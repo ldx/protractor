d7a3d8046b86934305c2ec27a83d3b68fdc6f1d0
Vilmos Nebehaj <v.nebehaj@gmail.com>
2014-05-30 17:04:00 +0200

    Use test user.

--- scripts/sauce_connect_setup.sh
+++ scripts/sauce_connect_setup.sh
@@ -46,7 +46,7 @@ echo "Starting Sauce Connect in the background, logging into:"
 echo "  $CONNECT_LOG"
 echo "  $CONNECT_STDOUT"
 echo "  $CONNECT_STDERR"
-sauce-connect/bin/sc -u test -k 56aa8e04-155c-4b5c-bcbc-a85547e8d70a -v \
+sauce-connect/bin/sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY -v \
   -x http://vilmos.dev.saucelabs.net/rest/v1 --vm-version test \
   --logfile $CONNECT_LOG $ARGS 2> $CONNECT_STDERR 1> $CONNECT_STDOUT &
 tail -f $CONNECT_STDOUT &
